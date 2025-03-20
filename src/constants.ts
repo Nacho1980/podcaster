a50e3e175adb49a8628a311bafe9637bad1bbcd8
Ignacio <ignacio.santos.alonso@gmail.com>
2025-03-20 21:04:36 +0100

    Trying to fix CORS issue in Vercel

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const PROXY2 = "https://thingproxy.freeboard.io/fetch/"; //used for podca
 export const TOP_PODCASTS_URL =
   "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
 export const PODCAST_DETAILS_URL =
-  "https://itunes.apple.com/lookup?id={podcastId}&country=US&media=podcast&entity=podcastEpisode&limit=100";
+  "https://itunes.apple.com/lookup?id={podcastId}&country=US&media=podcast&entity=podcastEpisode&limit=100/json";
 export const TOP_PODCASTS_LOCAL_STORAGE_KEY = "topPodcasts";
 export const PODCAST_DETAILS_LOCAL_STORAGE_KEY = "topPodcasts";
 export const CACHE_EXPIRES_MS = 24 * 60 * 60 * 1000; //1 day in millisecs
